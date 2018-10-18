0e98d9afaa65223f28c32dc86adaae65b093deb4
Joern Bernhardt <joern.bernhardt@googlemail.com>
2018-10-18 23:19:46 +0200

    Move library code into own module
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/diff/mod.rs
@@ -0,0 +1,50 @@
+extern crate keepass;
+
+use self::keepass::{Database, Group, OpenDBError};
+use std::fs::File;
+use std::path::Path;
+
+pub fn kdbx_to_sorted_vec(
+  file: &str,
+  password: &str,
+) -> Vec<(Vec<String>, Option<String>, Option<String>, Option<String>)> {
+  let db = File::open(Path::new(file))
+    .map_err(|e| OpenDBError::from(e))
+    .and_then(|mut db_file| Database::open(&mut db_file, password))
+    .unwrap();
+
+  accumulate_all_entries(db.root)
+}
+
+fn accumulate_all_entries(
+  start: Group,
+) -> Vec<(Vec<String>, Option<String>, Option<String>, Option<String>)> {
+  let mut accumulated = check_group(&mut Vec::new(), &mut Vec::new(), start);
+  accumulated.sort();
+  accumulated.dedup();
+  accumulated
+}
+
+fn check_group(
+  accumulated: &mut Vec<(Vec<String>, Option<String>, Option<String>, Option<String>)>,
+  parents: &mut Vec<String>,
+  current_group: Group,
+) -> Vec<(Vec<String>, Option<String>, Option<String>, Option<String>)> {
+  parents.push(current_group.name);
+  for entry in current_group.entries {
+    accumulated.push((
+      parents.clone(),
+      entry.get_title().map(|x| x.to_string()),
+      entry.get_username().map(|x| x.to_string()),
+      entry.get_password().map(|x| x.to_string()),
+    ))
+  }
+  let mut all_groups_children: Vec<(Vec<String>, Option<String>, Option<String>, Option<String>)> =
+    Vec::new();
+  for next_parent in current_group.child_groups {
+    let children = check_group(&mut accumulated.clone(), &mut parents.clone(), next_parent);
+    all_groups_children.append(&mut children.clone())
+  }
+  accumulated.append(&mut all_groups_children);
+  accumulated.clone()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
-extern crate keepass;
 extern crate rpassword;
 extern crate termcolor;
 
-use keepass::{Database, Group, OpenDBError};
+mod diff;
+
 use std::cmp::max;
 use std::env;
-use std::fs::File;
-use std::path::Path;
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 fn main() {
@@ -30,8 +28,8 @@ fn main() {
 }
 
 fn compare(file_a: &str, password_a: &str, file_b: &str, password_b: &str) {
-    let a = kdbx_to_sorted_vec(file_a, password_a);
-    let b = kdbx_to_sorted_vec(file_b, password_b);
+    let a = diff::kdbx_to_sorted_vec(file_a, password_a);
+    let b = diff::kdbx_to_sorted_vec(file_b, password_b);
 
     let maximum = max(a.len(), b.len());
     let mut a_idx = 0;
@@ -64,53 +62,4 @@ fn compare(file_a: &str, password_a: &str, file_b: &str, password_b: &str) {
         }
     }
     stdout.set_color(ColorSpec::new().set_fg(None)).unwrap();
-}
-
-fn kdbx_to_sorted_vec(
-    file: &str,
-    password: &str,
-) -> Vec<(Vec<String>, Option<String>, Option<String>, Option<String>)> {
-    let db = File::open(Path::new(file))
-        .map_err(|e| OpenDBError::from(e))
-        .and_then(|mut db_file| Database::open(&mut db_file, password))
-        .unwrap();
-
-    accumulate_all_entries(db.root)
-}
-
-fn accumulate_all_entries(
-    start: Group,
-) -> Vec<(Vec<String>, Option<String>, Option<String>, Option<String>)> {
-    let mut accumulated = check_group(&mut Vec::new(), &mut Vec::new(), start);
-    accumulated.sort();
-    accumulated.dedup();
-    accumulated
-}
-
-fn check_group(
-    accumulated: &mut Vec<(Vec<String>, Option<String>, Option<String>, Option<String>)>,
-    parents: &mut Vec<String>,
-    current_group: Group,
-) -> Vec<(Vec<String>, Option<String>, Option<String>, Option<String>)> {
-    parents.push(current_group.name);
-    for entry in current_group.entries {
-        accumulated.push((
-            parents.clone(),
-            entry.get_title().map(|x| x.to_string()),
-            entry.get_username().map(|x| x.to_string()),
-            entry.get_password().map(|x| x.to_string()),
-        ))
-    }
-    let mut all_groups_children: Vec<(
-        Vec<String>,
-        Option<String>,
-        Option<String>,
-        Option<String>,
-    )> = Vec::new();
-    for next_parent in current_group.child_groups {
-        let children = check_group(&mut accumulated.clone(), &mut parents.clone(), next_parent);
-        all_groups_children.append(&mut children.clone())
-    }
-    accumulated.append(&mut all_groups_children);
-    accumulated.clone()
 }
