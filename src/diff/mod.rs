4ea5d97cc22277842d3ba6ca6581928b9dd69a54
Stefan Seemayer <stefan@seemayer.de>
2019-10-12 14:22:53 +0200

    rustfmt

--- src/diff/mod.rs
+++ src/diff/mod.rs
@@ -9,95 +9,95 @@ type KdbxEntry = (Vec<String>, Option<String>, Option<String>, Option<String>);
 type SortedKdbxEntries = Vec<KdbxEntry>;
 
 pub enum ComparedEntry<T> {
-  Both(T),
-  OnlyLeft(T),
-  OnlyRight(T),
+    Both(T),
+    OnlyLeft(T),
+    OnlyRight(T),
 }
 
 pub fn compare(left: SortedKdbxEntries, right: SortedKdbxEntries) -> Vec<ComparedEntry<KdbxEntry>> {
-  let maximum = max(left.len(), right.len());
-  let mut left_idx = 0;
-  let mut right_idx = 0;
+    let maximum = max(left.len(), right.len());
+    let mut left_idx = 0;
+    let mut right_idx = 0;
 
-  let mut acc = Vec::<ComparedEntry<KdbxEntry>>::new();
-  while left_idx < maximum && right_idx < maximum {
-    let left_elem = left.get(left_idx);
-    let right_elem = right.get(right_idx);
-    match (left_elem, right_elem) {
-      (Some(a), Some(b)) => {
-        if a < b {
-          left_idx = left_idx + 1;
-          acc.push(ComparedEntry::OnlyLeft(a.clone()));
-        } else if b < a {
-          right_idx = right_idx + 1;
-          acc.push(ComparedEntry::OnlyRight(b.clone()));
-        } else {
-          left_idx = left_idx + 1;
-          right_idx = right_idx + 1;
-          acc.push(ComparedEntry::Both(a.clone()));
+    let mut acc = Vec::<ComparedEntry<KdbxEntry>>::new();
+    while left_idx < maximum && right_idx < maximum {
+        let left_elem = left.get(left_idx);
+        let right_elem = right.get(right_idx);
+        match (left_elem, right_elem) {
+            (Some(a), Some(b)) => {
+                if a < b {
+                    left_idx = left_idx + 1;
+                    acc.push(ComparedEntry::OnlyLeft(a.clone()));
+                } else if b < a {
+                    right_idx = right_idx + 1;
+                    acc.push(ComparedEntry::OnlyRight(b.clone()));
+                } else {
+                    left_idx = left_idx + 1;
+                    right_idx = right_idx + 1;
+                    acc.push(ComparedEntry::Both(a.clone()));
+                }
+            }
+            (Some(a), None) => {
+                left_idx = left_idx + 1;
+                acc.push(ComparedEntry::OnlyLeft(a.clone()));
+            }
+            (None, Some(b)) => {
+                right_idx = right_idx + 1;
+                acc.push(ComparedEntry::OnlyRight(b.clone()));
+            }
+            (None, None) => {
+                break;
+            }
         }
-      }
-      (Some(a), None) => {
-        left_idx = left_idx + 1;
-        acc.push(ComparedEntry::OnlyLeft(a.clone()));
-      }
-      (None, Some(b)) => {
-        right_idx = right_idx + 1;
-        acc.push(ComparedEntry::OnlyRight(b.clone()));
-      }
-      (None, None) => {
-        break;
-      }
     }
-  }
-  acc
+    acc
 }
 
 pub fn kdbx_to_sorted_vec(
-  file: &str,
-  password: Option<String>,
-  keyfile_path: Option<&str>,
+    file: &str,
+    password: Option<String>,
+    keyfile_path: Option<&str>,
 ) -> Result<SortedKdbxEntries> {
-  let mut keyfile = keyfile_path.map(|path| File::open(Path::new(path)).unwrap());
-  File::open(Path::new(file))
-    .map_err(|e| Error::from(e))
-    .and_then(|mut db_file| {
-      let db = Database::open(
-        &mut db_file,
-        password.as_ref().map(|s| s.as_str()),
-        keyfile.as_mut().map(|f| f as &mut dyn Read),
-      );
-      db
-    })
-    .map(|db: Database| accumulate_all_entries(db.root))
+    let mut keyfile = keyfile_path.map(|path| File::open(Path::new(path)).unwrap());
+    File::open(Path::new(file))
+        .map_err(|e| Error::from(e))
+        .and_then(|mut db_file| {
+            let db = Database::open(
+                &mut db_file,
+                password.as_ref().map(|s| s.as_str()),
+                keyfile.as_mut().map(|f| f as &mut dyn Read),
+            );
+            db
+        })
+        .map(|db: Database| accumulate_all_entries(db.root))
 }
 
 fn accumulate_all_entries(start: Group) -> SortedKdbxEntries {
-  let mut accumulated = check_group(&mut Vec::new(), &mut Vec::new(), start);
-  accumulated.sort();
-  accumulated.dedup();
-  accumulated
+    let mut accumulated = check_group(&mut Vec::new(), &mut Vec::new(), start);
+    accumulated.sort();
+    accumulated.dedup();
+    accumulated
 }
 
 fn check_group(
-  accumulated: &mut Vec<KdbxEntry>,
-  parents: &mut Vec<String>,
-  current_group: Group,
+    accumulated: &mut Vec<KdbxEntry>,
+    parents: &mut Vec<String>,
+    current_group: Group,
 ) -> Vec<KdbxEntry> {
-  parents.push(current_group.name);
-  for (_, entry) in current_group.entries {
-    accumulated.push((
-      parents.clone(),
-      entry.get_title().map(|x| x.to_string()),
-      entry.get_username().map(|x| x.to_string()),
-      entry.get_password().map(|x| x.to_string()),
-    ))
-  }
-  let mut all_groups_children = Vec::<KdbxEntry>::new();
-  for (_, next_parent) in current_group.child_groups {
-    let children = check_group(&mut accumulated.clone(), &mut parents.clone(), next_parent);
-    all_groups_children.append(&mut children.clone())
-  }
-  accumulated.append(&mut all_groups_children);
-  accumulated.clone()
+    parents.push(current_group.name);
+    for (_, entry) in current_group.entries {
+        accumulated.push((
+            parents.clone(),
+            entry.get_title().map(|x| x.to_string()),
+            entry.get_username().map(|x| x.to_string()),
+            entry.get_password().map(|x| x.to_string()),
+        ))
+    }
+    let mut all_groups_children = Vec::<KdbxEntry>::new();
+    for (_, next_parent) in current_group.child_groups {
+        let children = check_group(&mut accumulated.clone(), &mut parents.clone(), next_parent);
+        all_groups_children.append(&mut children.clone())
+    }
+    accumulated.append(&mut all_groups_children);
+    accumulated.clone()
 }
--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,14 @@ mod diff;
 use apply::apply;
 use clap::{App, Arg};
 use diff::{
-  compare, kdbx_to_sorted_vec,
-  ComparedEntry::{OnlyLeft, OnlyRight},
+    compare, kdbx_to_sorted_vec,
+    ComparedEntry::{OnlyLeft, OnlyRight},
 };
 use keepass::result::Result;
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 fn main() -> Result<()> {
-  let matches = App::new("keepass-diff")
+    let matches = App::new("keepass-diff")
     .version("0.3.0")
     .about("Shows differences between two .kdbx files")
     .author("Joern Bernhardt")
@@ -95,103 +95,103 @@ fn main() -> Result<()> {
     )
     .get_matches();
 
-  match (matches.value_of("INPUT-A"), matches.value_of("INPUT-B")) {
-    (Some(file_a), Some(file_b)) => {
-      let pass_a = match (
-        matches.value_of("password-a"),
-        matches.value_of("passwords"),
-        matches.is_present("no-password-a"),
-        matches.is_present("no-passwords"),
-      ) {
-        (Some(password), _, _, _) => Some(String::from(password)),
-        (_, Some(password), _, _) => Some(String::from(password)),
-        (_, _, true, _) => None,
-        (_, _, _, true) => None,
-        _ => {
-          print!("Password for file {}: ", file_a);
-          let password = rpassword::prompt_password_stdout("")
-            .map(|s| if s == "" { None } else { Some(s) })
-            .unwrap_or(None);
-          password
+    match (matches.value_of("INPUT-A"), matches.value_of("INPUT-B")) {
+        (Some(file_a), Some(file_b)) => {
+            let pass_a = match (
+                matches.value_of("password-a"),
+                matches.value_of("passwords"),
+                matches.is_present("no-password-a"),
+                matches.is_present("no-passwords"),
+            ) {
+                (Some(password), _, _, _) => Some(String::from(password)),
+                (_, Some(password), _, _) => Some(String::from(password)),
+                (_, _, true, _) => None,
+                (_, _, _, true) => None,
+                _ => {
+                    print!("Password for file {}: ", file_a);
+                    let password = rpassword::prompt_password_stdout("")
+                        .map(|s| if s == "" { None } else { Some(s) })
+                        .unwrap_or(None);
+                    password
+                }
+            };
+            let pass_b = match (
+                matches.value_of("password-b"),
+                matches.value_of("passwords"),
+                matches.is_present("no-password-b"),
+                matches.is_present("no-passwords"),
+            ) {
+                (Some(password), _, _, _) => Some(String::from(password)),
+                (_, Some(password), _, _) => Some(String::from(password)),
+                (_, _, true, _) => None,
+                (_, _, _, true) => None,
+                _ => {
+                    print!("Password for file {}: ", file_b);
+                    let password_option: Option<String> = rpassword::prompt_password_stdout("")
+                        .map(|s| if s == "" { None } else { Some(s) })
+                        .unwrap_or(None);
+                    password_option
+                }
+            };
+            let keyfile_a: Option<&str> = matches
+                .value_of("keyfile-a")
+                .or(matches.value_of("keyfiles"));
+            let keyfile_b: Option<&str> = matches
+                .value_of("keyfile-b")
+                .or(matches.value_of("keyfiles"));
+            let no_color: bool = matches.is_present("no-color");
+            run_comparison(
+                &file_a, pass_a, &file_b, pass_b, !no_color, keyfile_a, keyfile_b,
+            )
         }
-      };
-      let pass_b = match (
-        matches.value_of("password-b"),
-        matches.value_of("passwords"),
-        matches.is_present("no-password-b"),
-        matches.is_present("no-passwords"),
-      ) {
-        (Some(password), _, _, _) => Some(String::from(password)),
-        (_, Some(password), _, _) => Some(String::from(password)),
-        (_, _, true, _) => None,
-        (_, _, _, true) => None,
-        _ => {
-          print!("Password for file {}: ", file_b);
-          let password_option: Option<String> = rpassword::prompt_password_stdout("")
-            .map(|s| if s == "" { None } else { Some(s) })
-            .unwrap_or(None);
-          password_option
-        }
-      };
-      let keyfile_a: Option<&str> = matches
-        .value_of("keyfile-a")
-        .or(matches.value_of("keyfiles"));
-      let keyfile_b: Option<&str> = matches
-        .value_of("keyfile-b")
-        .or(matches.value_of("keyfiles"));
-      let no_color: bool = matches.is_present("no-color");
-      run_comparison(
-        &file_a, pass_a, &file_b, pass_b, !no_color, keyfile_a, keyfile_b,
-      )
+        _ => println!("Need two .kdbx files as arguments"),
     }
-    _ => println!("Need two .kdbx files as arguments"),
-  }
 
-  Ok(())
+    Ok(())
 }
 
 fn run_comparison(
-  file_a: &str,
-  password_a: Option<String>,
-  file_b: &str,
-  password_b: Option<String>,
-  use_color: bool,
-  keyfile_a: Option<&str>,
-  keyfile_b: Option<&str>,
+    file_a: &str,
+    password_a: Option<String>,
+    file_b: &str,
+    password_b: Option<String>,
+    use_color: bool,
+    keyfile_a: Option<&str>,
+    keyfile_b: Option<&str>,
 ) {
-  kdbx_to_sorted_vec(file_a, password_a, keyfile_a)
-    .and_then(|a| kdbx_to_sorted_vec(file_b, password_b, keyfile_b).map(|b| (a, b)))
-    .map(apply(&compare))
-    .map(|r| {
-      let mut i = 0;
-      let mut stdout = StandardStream::stdout(ColorChoice::Always);
-      while i < r.len() {
-        match r.get(i) {
-          Some(OnlyLeft(elem)) => {
-            if use_color {
-              stdout
-                .set_color(ColorSpec::new().set_fg(Some(Color::Red)))
-                .unwrap();
+    kdbx_to_sorted_vec(file_a, password_a, keyfile_a)
+        .and_then(|a| kdbx_to_sorted_vec(file_b, password_b, keyfile_b).map(|b| (a, b)))
+        .map(apply(&compare))
+        .map(|r| {
+            let mut i = 0;
+            let mut stdout = StandardStream::stdout(ColorChoice::Always);
+            while i < r.len() {
+                match r.get(i) {
+                    Some(OnlyLeft(elem)) => {
+                        if use_color {
+                            stdout
+                                .set_color(ColorSpec::new().set_fg(Some(Color::Red)))
+                                .unwrap();
+                        }
+                        println!("- {:?}", elem)
+                    }
+                    Some(OnlyRight(elem)) => {
+                        if use_color {
+                            stdout
+                                .set_color(ColorSpec::new().set_fg(Some(Color::Green)))
+                                .unwrap();
+                        }
+                        println!("+ {:?}", elem)
+                    }
+                    _ => {}
+                }
+                i = i + 1;
             }
-            println!("- {:?}", elem)
-          }
-          Some(OnlyRight(elem)) => {
             if use_color {
-              stdout
-                .set_color(ColorSpec::new().set_fg(Some(Color::Green)))
-                .unwrap();
+                stdout.set_color(ColorSpec::new().set_fg(None)).unwrap();
             }
-            println!("+ {:?}", elem)
-          }
-          _ => {}
-        }
-        i = i + 1;
-      }
-      if use_color {
-        stdout.set_color(ColorSpec::new().set_fg(None)).unwrap();
-      }
-    })
-    .unwrap_or_else(|err| {
-      println!("{}", err);
-    });
+        })
+        .unwrap_or_else(|err| {
+            println!("{}", err);
+        });
 }
