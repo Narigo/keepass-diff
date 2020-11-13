4bceb81225630fc971ffe88af6c034bb52501bac
Joern Bernhardt <joern.bernhardt@compose.us>
2020-11-13 22:15:51 +0100

    Add test for kdbx 3.1
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- run-tests.sh
+++ run-tests.sh
@@ -80,4 +80,30 @@
   test_equal "first run should have same amount of plus lines as second run has minus lines" $amount_of_plus_03 $amount_of_minus_04
   test_equal "first run should have same amount of minus lines as second run has plus lines" $amount_of_minus_03 $amount_of_plus_04
 
+  echo "### Running test to open KDBX 3.1 files"
+  echo "# Run a <diff> b"
+  cargo run "$PWD/test/issue-24-kdbx-3.1/Test1.kdbx" "$PWD/test/issue-24-kdbx-3.1/Test2.kdbx" --password-a Test1 --password-b Test2 --no-color >"$PWD/target/test-result-05.txt"
+  echo "# Run b <diff> a"
+  cargo run "$PWD/test/issue-24-kdbx-3.1/Test2.kdbx" "$PWD/test/issue-24-kdbx-3.1/Test1.kdbx" --password-a Test2 --password-b Test1 --no-color >"$PWD/target/test-result-06.txt"
+
+  lines_of_run_05=$(cat target/test-result-05.txt | wc -l)
+  lines_of_run_06=$(cat target/test-result-06.txt | wc -l)
+
+  test_equal "first run should have same amount of lines as second run" $lines_of_run_05 $lines_of_run_06
+
+  amount_of_plus_05=$(cat target/test-result-05.txt | grep '^+' | wc -l)
+  amount_of_minus_05=$(cat target/test-result-05.txt | grep '^-' | wc -l)
+  amount_of_tilde_05=$(cat target/test-result-05.txt | grep '^~' | wc -l)
+  amount_of_plus_06=$(cat target/test-result-06.txt | grep '^+' | wc -l)
+  amount_of_minus_06=$(cat target/test-result-06.txt | grep '^-' | wc -l)
+  amount_of_tilde_06=$(cat target/test-result-06.txt | grep '^~' | wc -l)
+
+  test_gt "should output more than 0 plus lines" $amount_of_plus_05 0
+  test_gt "should output more than 0 minus lines" $amount_of_minus_05 0
+  test_equal "should output 0 tilde lines" $amount_of_tilde_05 0
+  test_equal "should output 0 tilde lines in second run as well" $amount_of_tilde_06 0
+
+  test_equal "first run should have same amount of plus lines as second run has minus lines" $amount_of_plus_05 $amount_of_minus_06
+  test_equal "first run should have same amount of minus lines as second run has plus lines" $amount_of_minus_05 $amount_of_plus_06
+
 )
