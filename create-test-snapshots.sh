468969d590d9ed7a344d54513034f20403a09236
Joern Bernhardt <joern.bernhardt@compose.us>
2022-01-31 01:05:57 +0100

    chore: Add script to create snapshots and additional snapshots
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

---
+++ create-test-snapshots.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export RUSTFLAGS="-C target-cpu=native"
+
+for dir in $(ls -1d test/test-*) ; do
+  IFS='_' read -a files <<< "$(basename $dir | cut -c6-)"
+  file_a=${files[0]}
+  file_b=${files[1]}
+
+  for result in "$dir"/* ; do
+    args=$(basename "${result}" | cut -c8-)
+
+    snapshot_dir="tmp_tests/snapshots/$dir"
+    mkdir -p "$snapshot_dir"
+    cargo run -- test/__fixtures__/${file_a}.kdbx test/__fixtures__/${file_b}.kdbx ${args} > "$snapshot_dir/result_${args}"
+  done
+done
