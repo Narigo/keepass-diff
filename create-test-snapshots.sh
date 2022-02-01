0a0ece10b2b756b30a46846e5cdb63abb03d9024
Joern Bernhardt <joern.bernhardt@compose.us>
2022-02-01 01:10:27 +0100

    chore: Fix temporary snapshot directory
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- create-test-snapshots.sh
+++ create-test-snapshots.sh
@@ -10,7 +10,7 @@ for dir in $(ls -1d test/test-*) ; do
   for result in "$dir"/* ; do
     args=$(basename "${result}" | cut -c8-)
 
-    snapshot_dir="tmp_tests/snapshots/$dir"
+    snapshot_dir="tmp-tests/snapshots/$dir"
     mkdir -p "$snapshot_dir"
     cargo run -- test/__fixtures__/${file_a}.kdbx test/__fixtures__/${file_b}.kdbx ${args} > "$snapshot_dir/result_${args}"
   done
